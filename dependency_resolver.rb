9837afab6b353c69773bfce25a7871f5b8fcc1e9
Yonatan Miller <brombacher1@gmail.com>
2019-06-03 02:55:40 +0200

    Recursively load tasks and their dependencies

---
+++ dependency_resolver.rb
@@ -0,0 +1,43 @@
+require 'json'
+
+class DependencyResolver
+  attr_reader :json_data
+  attr_accessor :bash_file
+
+
+  def initialize(file_path)
+    @json_data = JSON.parse(File.read(file_path))
+    @bash_file = File.open('tasks.sh', 'w')
+    @bash_file.puts "#!/usr/bin/env ruby\n\n"
+  end
+
+  def add(task)
+    return if finished_tasks.include?(task['name'])
+
+    task['requires']&.each { |name| add(find_task_by_name(name)) }
+    finished_tasks << task['name']
+    bash_file.puts task['command']
+  end
+
+  def sort
+    tasks.each(&method(:add))
+  end
+
+  private
+
+  def tasks
+    json_data['tasks']
+  end
+
+  def finished_tasks
+    self.class.finished_tasks
+  end
+
+  def self.finished_tasks
+    @finished_tasks ||= []
+  end
+
+  def find_task_by_name(name)
+    tasks.select {|h| h.values.include?(name) }.first
+  end
+end
---
+++ spec/dependency_resolver_spec.rb
@@ -0,0 +1,57 @@
+require './dependency_resolver.rb'
+require 'spec_helper.rb'
+
+RSpec.describe DependencyResolver do
+  let(:dependency) { described_class.new('./tasks.json') }
+
+  describe '#initialize' do
+    it 'raises error for missing json file' do
+      expect { described_class.new }.to raise_error ArgumentError
+    end
+
+    it 'initializes "bash headers"' do
+      @buffer = StringIO.new
+      @bash_header = "#!/usr/bin/env ruby\n\n"
+      allow(File).to receive(:open).with('tasks.sh','w').and_return(@buffer)
+
+      dependency
+      expect(@buffer.string).to eq @bash_header
+    end
+  end
+
+  describe '#add' do
+    let(:task_1) {{ 'name' => 'task-1', 'command' => 'command1' }}
+    let(:task_2) {{ 'name' => 'task-2', 'command' => 'command2' }}
+    let(:task_4) {{ 'name' => 'task-4', 'command' => 'command4' }}
+    let(:task_3) do
+      {
+        'name' => 'task-3',
+        'command' => 'command3',
+        'requires' => ['task-4']
+      }
+    end
+
+    before(:each) do
+      @buffer = StringIO.new
+      allow(File).to receive(:open).with('tasks.sh','w').and_return(@buffer)
+    end
+
+    it 'adds a single task' do
+      dependency.add(task_1)
+      expect(@buffer.string.scan('command1').count).to eq 1
+    end
+
+    it 'does not add task twice' do
+      2.times { dependency.add(task_2) }
+      expect(@buffer.string.scan('command1').count).to eq 0
+      expect(@buffer.string.scan('command2').count).to eq 1
+    end
+
+    it 'outputs tasks in dependent order' do
+      expect(dependency).to receive(:find_task_by_name).with('task-4').and_return(task_4)
+      dependency.add(task_3)
+
+      expect(@buffer.string.split("\n").last(2)).to eq (%w[command4 command3])
+    end
+  end
+end
